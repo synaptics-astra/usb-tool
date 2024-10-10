01fb8ab1086734674590582a3d6c8a94668d18da
Andrew Duggan <aduggan@synaptics.com>
2024-10-09 20:13:22 -0700

    Add usb_boot binary for raspberry pi os bookwork

--- usbboot_sl1640_uboot_rdk/run.sh
+++ usbboot_sl1640_uboot_rdk/run.sh
@@ -12,7 +12,11 @@ if [[ "${HOST_OS}" == "Darwin" ]]; then
 	else
 		USB_BOOT_BIN_PATH="./bin/mac/x86_64"
 	fi
+elif [[ "${HOST_OS}" == "Linux" ]]; then
+    if [[ "${HOST_ARCH}" == "aarch64" ]]; then
+        USB_BOOT_BIN_PATH="./bin/linux/arm64"
+    fi
 fi
 
 # sudo required to access USB devices and semaphores
-sudo ${USB_BOOT_BIN_PATH}/usb_boot 06CB 00B0 ./images/ 8141 turn_off_telnet
+sudo ${USB_BOOT_BIN_PATH}/usb_boot 06CB 00B1 ./images/ 8141 turn_off_telnet
