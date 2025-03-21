528ce2fa989373303e87ac35a2e2e11d506fe789
Andrew Duggan <aduggan@synaptics.com>
2025-03-20 22:01:09 -0700

    Update batch files to keep the terminal open on exit

--- update_spi.bat
+++ update_spi.bat
@@ -10,21 +10,15 @@ set "SL1680=sl1680"
 if exist "%SL1620%" (
     echo SL1620 SPI image exists...
     .\bin\win\astra-update -f "%SL1620%"
-    exit /b
-)
-
-if exist "%SL1640%" (
+) else if exist "%SL1640%" (
     echo SL1640 SPI image exists...
     .\bin\win\astra-update -f "%SL1640%"
-    exit /b
-)
-
-if exist "%SL1680%" (
+) else if exist "%SL1680%" (
     echo SL1680 SPI image exists...
     .\bin\win\astra-update -f "%SL1680%"
-    exit /b
+) else (
+    echo No SPI images detected. Download an image from https://github.com/synaptics-astra/spi-u-boot
 )
 
-echo No SPI images detected. Download an image from https://github.com/synaptics-astra/spi-u-boot
-exit /b
+pause
 
